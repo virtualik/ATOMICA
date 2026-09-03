e33c15664ab563984dee0a3a8f4cf340bafea5f7
virtualik <virtualik@users.noreply.github.com>
2026-09-03 13:02:27 +0000

    Deploying to gh-pages from @ virtualik/ATELIER@b1557376b8360e513730a7641102bc1823f1a70c 🚀

--- ALTAURI_Web.js
+++ ALTAURI_Web.js
@@ -49569,7 +49569,7 @@ var lime_utils_AssetCache = function() {
 	this.audio = new haxe_ds_StringMap();
 	this.font = new haxe_ds_StringMap();
 	this.image = new haxe_ds_StringMap();
-	this.version = 759486;
+	this.version = 763995;
 };
 $hxClasses["lime.utils.AssetCache"] = lime_utils_AssetCache;
 lime_utils_AssetCache.__name__ = "lime.utils.AssetCache";
