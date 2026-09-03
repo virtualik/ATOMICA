d389137e3ece7a312be727cd49685edaae3fc6e1
virtualik <atari.robot@gmail.com>
2026-09-03 15:39:46 +0000

    Deploying to gh-pages from @ virtualik/ATELIER@ebe559be815ef05a8d5d71bc6085e0f3ef3a9cb2 🚀

--- ALTAURI_Web.js
+++ ALTAURI_Web.js
@@ -49569,7 +49569,7 @@ var lime_utils_AssetCache = function() {
 	this.audio = new haxe_ds_StringMap();
 	this.font = new haxe_ds_StringMap();
 	this.image = new haxe_ds_StringMap();
-	this.version = 779139;
+	this.version = 235330;
 };
 $hxClasses["lime.utils.AssetCache"] = lime_utils_AssetCache;
 lime_utils_AssetCache.__name__ = "lime.utils.AssetCache";
